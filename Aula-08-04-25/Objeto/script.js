cdd3cd322e33744ff22dc4e6483a9f0833e94a53
Vinicios Trindade Costa <135274652+Vinhicious@users.noreply.github.com>
2025-04-08 11:23:13 -0300

    Add files via upload

---
+++ Aula-08-04-25/Objeto/script.js
@@ -0,0 +1,46 @@
+//objetos
+const pessoa = {
+    nome: "Fulano",
+    idade: 23,
+    altura: 1.85
+};
+
+console.log(pessoa);
+
+let pessoa2 = {};
+pessoa2.nome = "Ciclano";
+pessoa2.signo = "Touro";
+console.log(pessoa2);
+
+const carro = new Object();
+carro.marca = "Toyota";
+carro.ano = 2023;
+console.log(carro)
+
+function Pessoa(nome, idade){
+    this.nome = nome;
+    this.idade = idade;
+}
+
+let p1 = new Pessoa("João", 25);
+let p2 = new Pessoa("Maria", 23);
+
+console.log(p1.nome);
+console.log(p2);
+
+class Animal {
+    constructor(nome, tutor){
+        this.nome = nome;
+        this.tutor = tutor;
+    }
+
+    emitirSom(){
+        return `O ${this.nome} tem como tutor: ${this.tutor}`
+    }
+}
+
+let gato = new Animal("Felix", "Fulano");
+let cachorro = new Animal("Perro", "Ciclano");
+
+console.log(gato.emitirSom())
+console.log(cachorro.emitirSom())
